d0b27c3bf1bde9bd60f234b3e9d42050a51f5812
JayBhende05 <158417378+JayBhende05@users.noreply.github.com>
2025-10-18 22:40:15 +0530

    Integrated Prisma + Accelerate + Hono + Cloudflare

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,46 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
 import auth from './routes/authRoutes'
-import blog from './routes/blogRoutes'
+// import blog from './routes/blogRoutes'
 
-const app = new Hono()
+
+type Env = {
+  DATABASE_URL: string
+}
+
+const app = new Hono<{ Bindings: Env }>()
 
 app.route('/api/v1/auth/', auth )
-app.route('/api/v1/blog', blog)
+// app.route('/api/v1/blog', blog)
+
+
+app.get('/', async (c) => {
+  try {
+      console.log("BBBBStarting Prisma...")
+    const dbUrl = c.env.DATABASE_URL
+    console.log(dbUrl);
+    console.log("Starting Prisma...")
+
+    const prisma = new PrismaClient({
+      datasourceUrl: dbUrl,
+    }).$extends(withAccelerate())
+
+    const users = await prisma.user.findMany({
+      where: {
+        email: {
+          contains: "xyz@gmail.com",
+        },
+      },
+      cacheStrategy: { ttl: 60 },
+    })
+
+    return c.json(users)
+  } catch (err: any) {
+    console.error("Error querying users:", err)
+    return c.json({ error: err.message || "Internal Server Error" }, 500)
+  }
+})
+
 
 export default app
--- backend/src/routes/blogRoutes.ts
+++ backend/src/routes/blogRoutes.ts
@@ -5,7 +5,7 @@ blog.post('/', (c) => {return c.text("Blog Uploaded")})
 blog.put('/', (c) => {return c.text("Blog Editied")})
 blog.get('/:id' , (c) => { const {id} = c.req.param()
 return c.text(`User with id ${id}`)})
-blog.get('/bulk', (c) =>{ c.text("Blogs are presented in Blulk") })
+blog.get('/bulk', (c) =>{ return  c.text("Blogs are presented in Blulk") })
 
 
 export default blog
